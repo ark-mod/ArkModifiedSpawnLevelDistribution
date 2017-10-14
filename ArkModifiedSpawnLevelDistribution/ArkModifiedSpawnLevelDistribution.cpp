bee56236bc66fe4fd5b091c4d9251983274801b4
Tobias Sebring <tsebring@gmail.com>
2017-10-14 10:00:26 +0200

    Initial import

---
+++ ArkModifiedSpawnLevelDistribution/ArkModifiedSpawnLevelDistribution.cpp
@@ -0,0 +1,78 @@
+#include <windows.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <list>
+#include <thread>
+#include "API/Base.h"
+#include "ArkModifiedSpawnLevelDistribution.h"
+
+#pragma comment(lib, "ArkApi.lib")
+
+DECLARE_HOOK(APrimalDinoCharacter_GetRandomBaseLevel, int, APrimalDinoCharacter*, float);
+
+int _cdecl Hook_APrimalDinoCharacter_GetRandomBaseLevel(APrimalDinoCharacter* _APrimalDinoCharacter, float ForceRand_NotUsed)
+{
+	TArray<FDinoBaseLevelWeightEntry> baseLevelWeightEntries = _APrimalDinoCharacter->GetDinoBaseLevelWeightEntriesField();
+	//float npcLerpToMaxRandomBaseLevel = _APrimalDinoCharacter->GetNPCLerpToMaxRandomBaseLevelField();
+
+	if (baseLevelWeightEntries.Num() == 4
+		&& baseLevelWeightEntries[0].EntryWeight == 1.0
+		&& (baseLevelWeightEntries[0].BaseLevelMinRange == 1.0 || baseLevelWeightEntries[0].BaseLevelMinRange == 5.0)
+		&& baseLevelWeightEntries[0].BaseLevelMaxRange == 5.0
+
+		&& baseLevelWeightEntries[1].EntryWeight == 0.5
+		&& baseLevelWeightEntries[1].BaseLevelMinRange == 6.0
+		&& baseLevelWeightEntries[1].BaseLevelMaxRange == 12.0
+
+		&& baseLevelWeightEntries[2].EntryWeight == 0.25
+		&& baseLevelWeightEntries[2].BaseLevelMinRange == 13.0
+		&& baseLevelWeightEntries[2].BaseLevelMaxRange == 20.0
+
+		&& fabs(baseLevelWeightEntries[3].EntryWeight - 0.1) < 0.00001
+		&& baseLevelWeightEntries[3].BaseLevelMinRange == 21.0
+		&& baseLevelWeightEntries[3].BaseLevelMaxRange == 30.0)
+	{
+		baseLevelWeightEntries[0].EntryWeight = 1.0;
+		baseLevelWeightEntries[0].BaseLevelMinRange = 3.0;
+		baseLevelWeightEntries[0].BaseLevelMaxRange = 5.0;
+
+		baseLevelWeightEntries[1].EntryWeight = 0.75;
+		baseLevelWeightEntries[1].BaseLevelMinRange = 9.0;
+		baseLevelWeightEntries[1].BaseLevelMaxRange = 12.0;
+
+		baseLevelWeightEntries[2].EntryWeight = 0.625;
+		baseLevelWeightEntries[2].BaseLevelMinRange = 16.5;
+		baseLevelWeightEntries[2].BaseLevelMaxRange = 20.0;
+
+		baseLevelWeightEntries[3].EntryWeight = 0.55;
+		baseLevelWeightEntries[3].BaseLevelMinRange = 25.5;
+		baseLevelWeightEntries[3].BaseLevelMaxRange = 30.0;
+	}
+
+	int randomBaseLevel = APrimalDinoCharacter_GetRandomBaseLevel_original(_APrimalDinoCharacter, ForceRand_NotUsed);
+
+	return randomBaseLevel;
+}
+
+void Init()
+{
+	Ark::SetHook("APrimalDinoCharacter", "GetRandomBaseLevel", &Hook_APrimalDinoCharacter_GetRandomBaseLevel, reinterpret_cast<LPVOID*>(&APrimalDinoCharacter_GetRandomBaseLevel_original));
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		Init();
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# ARK Beyond API: Modified Spawn Level Distribution (Plugin)
+
+## Introduction
+
+Changes to spawn level distribution on The Island and Scorched Earth to match the official spawn level distribution on Ragnarok and The Center.
+
+## Installation
+
+* Install ARK Beyond API (forked by me) on your server.
+* Copy/extract plugin to `BeyondApi/Plugins/ArkModifiedSpawnLevelDistribution`.
+
+## Acknowledgements
+
+This plugin is based on Michidu's work on Ark-Server-Plugins and ARK Beyond API. The basic plumbing code is copied directly from those plugins.
+
+## Links
+
+My ARK Beyond API Fork (https://github.com/tsebring/ARK-Server-Beyond-API)
+
+ARK Beyond API by Michidu (https://github.com/Michidu/ARK-Server-Beyond-API)
+
+Ark-Server-Plugins (https://github.com/Michidu/Ark-Server-Plugins)
